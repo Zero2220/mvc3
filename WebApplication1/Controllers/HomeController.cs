80e292bc977a893cb8cc4fe583f9938a12e7ffe1
anarpi <anarpi@code.edu.az>
2024-04-18 18:01:37 +0400

    new

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+    }
+}
